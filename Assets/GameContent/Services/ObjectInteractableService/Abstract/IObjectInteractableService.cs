db357679116ffee223c0ff66088b5e37963a8321
Eugene <bogutskiy1983@gmail.com>
2022-05-21 16:45:58 +0300

    add InteractableService

---
+++ Assets/GameContent/Services/ObjectInteractableService/Abstract/IObjectInteractableService.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class IObjectInteractableService : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/GameContent/Services/ObjectInteractableService/ObjectInteractableService.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ObjectInteractableService : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
