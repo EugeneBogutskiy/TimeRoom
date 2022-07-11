2faf9a8d78f63977c3e3cc6571c3d2010cba7882
Eugene <bogutskiy1983@gmail.com>
2022-07-11 22:15:16 +0300

    Fix build action

--- Assets/GameContent/ScreenShotIconMaker/ScreenShotIconMaker.cs
+++ Assets/GameContent/ScreenShotIconMaker/ScreenShotIconMaker.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using GameContent.InventorySystem.SimpleInventorySystem;
-using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace GameContent.ScreenShotIconMaker
 {
     public class ScreenShotIconMaker : MonoBehaviour
     {
-        private readonly string _pathFolder = $"/GameContent/Art/UI/GeneratedIcons/";
-
-        [SerializeField]
-        private Camera _camera;
+        [SerializeField] private Camera _camera;
 
         public List<GameObject> sceneObjects;
         public List<InventoryItemData> dataObjects;
+        private readonly string _pathFolder = "/GameContent/Art/UI/GeneratedIcons/";
 
         [ContextMenu("TakeScreenShot")]
         public void ProcessScreenShots()
@@ -25,44 +25,44 @@ namespace GameContent.ScreenShotIconMaker
 
         private IEnumerator ScreenShot()
         {
-            for (int i = 0; i < sceneObjects.Count; i++)
+            for (var i = 0; i < sceneObjects.Count; i++)
             {
-                GameObject obj = sceneObjects[i];
-                InventoryItemData data = dataObjects[i];
-                
+#if UNITY_EDITOR
+
+                var obj = sceneObjects[i];
+                var data = dataObjects[i];
+
                 obj.gameObject.SetActive(true);
 
                 yield return null;
-                
+
                 TakeScreenShot($"{Application.dataPath}/{_pathFolder}/{data.id}_Icon.png");
 
                 yield return null;
-                
+
                 obj.gameObject.SetActive(false);
 
-                Sprite s = AssetDatabase.LoadAssetAtPath<Sprite>($"Assets/{_pathFolder}/{data.id}_Icon.png");
+                var s = AssetDatabase.LoadAssetAtPath<Sprite>($"Assets/{_pathFolder}/{data.id}_Icon.png");
 
                 if (s != null)
                 {
                     data.icon = s;
                     EditorUtility.SetDirty(data);
                 }
+#endif
 
                 yield return null;
             }
         }
-        
+
         private void TakeScreenShot(string fullPath)
         {
-            if (_camera is null)
-            {
-                _camera = GetComponent<Camera>();
-            }
+            if (_camera is null) _camera = GetComponent<Camera>();
 
-            RenderTexture renderTexture = new RenderTexture(256, 256, 24);
+            var renderTexture = new RenderTexture(256, 256, 24);
             _camera.targetTexture = renderTexture;
-            Texture2D screenShot = new Texture2D(256, 256, TextureFormat.RGBA32, false);
-            
+            var screenShot = new Texture2D(256, 256, TextureFormat.RGBA32, false);
+
             _camera.Render();
 
             RenderTexture.active = renderTexture;
@@ -71,22 +71,18 @@ namespace GameContent.ScreenShotIconMaker
             RenderTexture.active = null;
 
             if (Application.isEditor)
-            {
                 DestroyImmediate(renderTexture);
-            }
             else
-            {
                 Destroy(renderTexture);
-            }
 
-            byte[] bytes = screenShot.EncodeToPNG();
+            var bytes = screenShot.EncodeToPNG();
             File.WriteAllBytes(fullPath, bytes);
-            
+
             Debug.Log($"File saved in {fullPath}");
-            
-            #if UNITY_EDITOR
+
+#if UNITY_EDITOR
             AssetDatabase.Refresh();
-            #endif
+#endif
         }
     }
 }
